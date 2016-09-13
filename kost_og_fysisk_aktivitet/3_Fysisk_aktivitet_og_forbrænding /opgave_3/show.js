460ccba8814aa1bab512bb1191dd20e2ca5d3b13
Karoline Krog Vind <kakv@Jacobs-iMac.local>
2016-09-13 14:55:59 +0200

    Oprettet mappe til de overførte objekter fra Sharepoint

---
+++ kost_og_fysisk_aktivitet/3_Fysisk_aktivitet_og_forbrænding /opgave_3/show.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+	$("#flashContent_mkj").toggle();
+
+	$("#switch").click(function() {
+		$("#flashContent_mkj").toggle();
+		$("#flashContent_ukj").toggle();
+
+	});
+});
