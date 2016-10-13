a0629025ae3f0af125ec78a137707e6a93cb72a2
Karoline Krog Vind <kakv@Jacobs-iMac.local>
2016-10-13 12:44:26 +0200

    Tilpasning af objekter og oprydning af mappe i del 2

--- kost_og_fysisk_aktivitet/3_fysisk_aktivitet_og_forbraending /opgave_3/show.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-	$("#flashContent_mkj").toggle();
-
-	$("#switch").click(function() {
-		$("#flashContent_mkj").toggle();
-		$("#flashContent_ukj").toggle();
-
-	});
-});
